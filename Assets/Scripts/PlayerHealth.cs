b8352954f7aa653a52bf2387bff68e3f708008c9
Seb <sebastian20so08@gmail.com>
2026-03-05 13:33:08 +0100

    Added Sound Files

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,4 @@
 using UnityEngine;
-
-public class PlayerHealth : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-using UnityEngine;
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
