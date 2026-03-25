801e01d0df360d28ef4576fbf1b6e0617e5e9af4
Stra6767 <strahinja09@outlook.com>
2026-03-25 12:43:09 +0100

    enemystats

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -84,3 +85,91 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject); // removes the enemy from the scene
     }
 }
+=======
+using System;
+
+// basic enemy class with simple stats
+public class Enemy
+{
+    // variables for enemy info
+    public int maxHealth;
+    public int currentHealth;
+    public int attack;
+    public int defense;
+    public float speed;
+
+    // constructor to set up the enemy when created
+    public Enemy(string n, int hp, int dmg, int def, float spd)
+    {
+        maxHealth = hp;
+        currentHealth = hp;
+        attack = dmg;
+        defense = def;
+        speed = spd;
+    }
+
+    // method for taking damage
+    public void TakeDamage(int dmg)
+    {
+        // reduce incoming damage by defense
+        int realDmg = dmg - defense;
+
+        // make sure damage doesn't go negative
+        if (realDmg < 0)
+            realDmg = 0;
+
+        currentHealth -= realDmg;
+
+        Console.WriteLine(name + " took " + realDmg + " damage. HP: " + currentHealth + "/" + maxHealth);
+
+        // check if enemy died
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // method for attacking something
+    public int Attack()
+    {
+        Console.WriteLine(name + " attacks for " + attack + " damage");
+        return attack;
+    }
+
+    // what happens when HP hits 0
+    void Die()
+    {
+        currentHealth = 0;
+        Console.WriteLine(name + " is dead now");
+    }
+
+    // prints out the enemy's stats
+    public void PrintStats()
+    {
+        Console.WriteLine("----- Enemy Stats -----");
+        Console.WriteLine("HP: " + currentHealth + "/" + maxHealth);
+        Console.WriteLine("Attack: " + attack);
+        Console.WriteLine("Defense: " + defense);
+        Console.WriteLine("Speed: " + speed);
+        Console.WriteLine("------------------------");
+    }
+}
+
+// simple test program to try out the enemy
+public class Program
+{
+    public static void Main()
+    {
+        // creating a bat enemy
+        Enemy bat = new Enemy("Bat", 40, 6, 1, 4.2f);
+
+        bat.PrintStats();
+        bat.TakeDamage(12);
+        bat.Attack();
+        bat.TakeDamage(100);
+        bat.PrintStats();
+
+        Console.ReadKey();
+    }
+}
+>>>>>>> Stashed changes
